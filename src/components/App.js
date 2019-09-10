3e9d4caf23116fcd86fb97c801afc999a2496048
Priz Almarinez <priz.almarinez@gmail.com>
2019-09-10 13:31:13 +0800

    - added post components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+// components
+import PostList from './posts/PostList'
 
 const App = () => {
     return (
         <div>
-        App      
+            <PostList />
         </div>
     );
 };
---
+++ src/components/posts/AddPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddPost extends Component {
+    render() {
+        return (
+            <div>
+                Add Post
+            </div>
+        );
+    }
+}
+
+export default AddPost;
